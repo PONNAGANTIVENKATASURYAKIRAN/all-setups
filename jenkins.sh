068258f4bbb7abc04d9aea6b63c440f0a338014b
PONNAGANTIVENKATASURYAKIRAN <155225883+PONNAGANTIVENKATASURYAKIRAN@users.noreply.github.com>
2026-07-07 06:13:19 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,6 @@
 
 #! /bin/bash
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 # Add required dependencies for the jenkins package
